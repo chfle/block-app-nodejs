bc0f5ce10a3532021ee25e321ab65742b65fc64b
c <Christian.lehnert04@icloud.com>
2019-01-02 00:03:16 -0800

    added redis to the project

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -14,9 +14,30 @@ module.exports = (app) => {
   });
 
   app.get('/api/blogs', requireLogin, async (req, res) => {
+    const redis = require('redis');
+    const util = require('util');
+    const redisUrl = 'redis://127.0.0.1:6379';
+    const client = redis.createClient(redisUrl);
+
+    // change client.get from callback to promise
+    client.get = util.promisify(client.get);
+
+    // Do we have any cached data in redis related to this query
+    const cachedBlogs = await client.get(req.user.id);
+
+
+    // if yes, return,
+    if (cachedBlogs) {
+      console.log('Redis was called not mongoDB');
+      return res.send(JSON.parse(cachedBlogs));
+    }
+
+    // if no, update cache
     const blogs = await Blog.find({ _user: req.user.id });
 
     res.send(blogs);
+    console.log('Mongodb was called not redis');
+    client.set(req.user.id, JSON.stringify(blogs));
   });
 
   app.post('/api/blogs', requireLogin, async (req, res) => {

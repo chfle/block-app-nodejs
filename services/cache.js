a74befe584adca3dc8364c9cf75766351a588f07
c <Christian.lehnert04@icloud.com>
2019-01-02 23:44:16 -0800

    modify moongoose to use redis

--- services/cache.js
+++ services/cache.js
@@ -3,7 +3,7 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 
-const util = redis('util');
+const util = require('util');
 
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
@@ -11,17 +11,28 @@ client.get = util.promisify(client.get);
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
-  const key = Object.assign({}, this.getQuery(), {
+mongoose.Query.prototype.exec = async function () {
+  const key = JSON.stringify(Object.assign({}, this.getQuery(), {
     collection: this.mongooseCollection.name,
-  });
+  }));
 
   // See if we have a value for 'key' in redis
+  const cacheValue = await client.get(key);
 
   // if return that
+  if (cacheValue) {
+    const doc = JSON.parse(cacheValue);
 
+    // create a model for each obj or array
+    return Array.isArray(doc)
+      ? doc.map(d => new this.model(d))
+      : new this.model(doc);
+  }
 
   // otherwise issue the query and store it in redis
+  const result = await exec.apply(this, arguments);
 
-  return exec.apply(this, arguments);
+  client.set(key, JSON.stringify(result));
+
+  return result;
 };

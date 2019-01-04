1588bf800c3edd6dd17c84ac72b679f4c66102c2
c <Christian.lehnert04@icloud.com>
2019-01-03 23:25:24 -0800

    testing header

---
+++ tests/header.test.js
@@ -0,0 +1,22 @@
+const puppeteer = require('puppeteer');
+
+let browser;
+let page;
+
+beforeEach(async () => {
+  browser = await puppeteer.launch({
+    headless: false,
+  });
+  page = await browser.newPage();
+  await page.goto('localhost:3000');
+});
+
+afterEach(async () => {
+  await browser.close();
+});
+
+test('should show the app logo in the header', async () => {
+  const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+
+  expect(text).toEqual('Blogster');
+});

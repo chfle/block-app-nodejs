0d70d81913f06b9ed46ebac922219ecda07877cf
c <Christian.lehnert04@icloud.com>
2019-01-05 23:13:13 -0800

    testing login

--- tests/header.test.js
+++ tests/header.test.js
@@ -11,9 +11,9 @@ beforeEach(async () => {
   await page.goto('localhost:3000');
 });
 
-afterEach(async () => {
-  await browser.close();
-});
+// afterEach(async () => {
+//   await browser.close();
+// });
 
 test('should show the app logo in the header', async () => {
   const text = await page.$eval('a.brand-logo', el => el.innerHTML);
@@ -21,9 +21,36 @@ test('should show the app logo in the header', async () => {
   expect(text).toEqual('Blogster');
 });
 
+test('should show login button', async () => {
+  const loginButton = await page.$eval('.right a', el => el.innerHTML);
+  expect(loginButton).toEqual('Login With Google');
+});
+
 test('click login, should start oauth flow', async () => {
   await page.click('.right a');
 
   const url = await page.url();
   expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test.only('show logout button when signed in', async () => {
+  const id = '5c2b288bb27b581c767b0fad';
+
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id,
+    },
+  };
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign(`session=${sessionString}`);
+
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
 });

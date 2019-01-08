61197f6ccd6cb03c09362ce4d8fbd3b70429e3f6
c <Christian.lehnert04@icloud.com>
2019-01-08 00:02:38 -0800

    testing for blogs when logged in

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -16,9 +16,46 @@ describe('When logged in', async () => {
     await page.login();
     await page.click('a.btn-floating');
   }));
+
   test('should show blog create form', async () => {
     const titleLabel = await page.getContentOf('.title label');
 
     expect(titleLabel).toEqual('Blog Title');
+  });
+
+  describe('and using valid inputs', async () => {
+    beforeEach(async () => {
+      await page.type('.title input', 'My Title');
+      await page.type('.content input', 'My Content');
+      await page.click('form button');
+    });
+    test('than should take user to review screen', async () => {
+      const text = await page.getContentOf('h5');
+
+      expect(text).toEqual('Please confirm your entries');
+    });
+    test('than should save new blog to the index page', async () => {
+      await page.click('button.green');
+      await page.waitFor('.card');
+
+      const title = await page.getContentOf('.card-title');
+      const content = await page.getContentOf('p');
+
+      expect(title).toEqual('My Title');
+      expect(content).toEqual('My Content');
+    });
+  });
+
+  describe('And using invalid input', async () => {
+    beforeEach(async () => {
+      await page.click('form button');
+    });
+    test('form should show an error message', async () => {
+      const titleError = await page.getContentOf('.title .red-text');
+      const contentError = await page.getContentOf('.content .red-text');
+
+      expect(titleError).toEqual('You must provide a value');
+      expect(contentError).toEqual('You must provide a value');
+    });
   });
 });

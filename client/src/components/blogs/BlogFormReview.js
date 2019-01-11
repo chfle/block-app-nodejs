861f1c61a3e27fc053f573b2d1475a5b5ad70d58
c <Christian.lehnert04@icloud.com>
2019-01-11 00:15:09 -0800

    setup for aws

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -2,22 +2,39 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import formFields from './formFields';
 import { withRouter } from 'react-router-dom';
+import formFields from './formFields';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { file: null };
+    this.onFileChange = this.onFileChange.bind(this);
+  }
+
+  onFileChange(e) {
+    this.setState({ file: e.target.value.files[0] });
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+
+    const { submitBlog, history, formValues } = this.props;
+    const { file } = this.state;
+
+    submitBlog(formValues, file, history);
+  }
+
   renderFields() {
     const { formValues } = this.props;
 
-    return _.map(formFields, ({ name, label }) => {
-      return (
-        <div key={name}>
-          <label>{label}</label>
-          <div>{formValues[name]}</div>
-        </div>
-      );
-    });
+    return _.map(formFields, ({ name, label }) => (
+      <div key={name}>
+        <label>{label}</label>
+        <div>{formValues[name]}</div>
+      </div>
+    ));
   }
 
   renderButtons() {
@@ -39,19 +56,19 @@ class BlogFormReview extends Component {
     );
   }
 
-  onSubmit(event) {
-    event.preventDefault();
-
-    const { submitBlog, history, formValues } = this.props;
-
-    submitBlog(formValues, history);
-  }
 
   render() {
     return (
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
+
+        <h5>Add An image</h5>
+        <input
+          onChange={this.onFileChange}
+          type="file"
+          accept="image/*"
+        />
 
         {this.renderButtons()}
       </form>
